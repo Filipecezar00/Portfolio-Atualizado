8de807e274fca866adcae55241464906a109aa54
Filipe Cezar <cezarfilipe01@gmail.com>
2026-01-21 20:45:03 -0300

    atualizando

--- Portfolio/main.js
+++ Portfolio/main.js
@@ -11,6 +11,8 @@ const translations = {
         sobremim:"Sobre mim", 
         projetos:"Projetos", 
         habilidades:"Habilidades", 
+        btn_dark:"Escuro", 
+        btn_light:"Claro", 
         button:"Traduzir para EN", 
         title:"Olá, sou Filipe, Estagiário em Desenvolvimento" ,
         about:"Estudante de Sistemas da informação, com foco em desenvolvimento de software e forte interesse em criação de softwares e aplicações web. Possuo conhecimentos em MongoDB, Node js e SQL,adquiridos por meio de projetos pessoais e acadêmicos.Tenho familiaridade com APIs REST, versionamento de código e boas práticas de programação Atualmente, atuo como Jovem Aprendiz de TI na Monte Rodovias , onde desenvolvo habilidades práticas em suporte e infraestrutura. Com Inglês nível B2, busco constantemente me atualizar através de documentações internacionais e comunidades globais, com o objetivo de evoluir na área de Desenvolvimento"
@@ -18,12 +20,18 @@ const translations = {
     en:{
         sobremim:"About me", 
         projetos:"Projects", 
-        habilidades:"Skills", 
+        habilidades:"Skills",
+        btn_dark:"Dark", 
+        btn_light:"Light",  
         button:"Translate to PT", 
         title:"Hi, I am Filipe, a Software Development Intern", 
         about:"Systems Information Student With a strong focus on Software development and web applications. I have experience with MongoDB, Node.js, and SQL.... Currently, I work as a IT junior Apprentice at Monte Rodovias, My english is B2 Level, my goal is use it to grow my carrer in Development."
     }
 }; 
+
+const btn_dark = document.getElementById("btn_dark"); 
+const btn_light = document.getElementById("btn_light") 
+
 
 const sobremim = document.getElementById("sobremim");
 const habilidades = document.getElementById("habilidades");
@@ -39,6 +47,9 @@ let currentLang = "pt";
 langButton.addEventListener("click",()=>{
     currentLang = currentLang === "pt" ? "en" : "pt"; 
 
+    btn_dark.innerText = translations[currentLang].btn_dark
+    btn_light.innerText = translations[currentLang].btn_light
+    
     sobremim.innerText = translations[currentLang].sobremim 
     habilidades.innerText = translations[currentLang].habilidades 
     projetos.innerText = translations[currentLang].projetos 
